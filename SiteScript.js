0b19c274859390479ef3b5043ba90f94752cd0f4
PS5Australia <98734886+PS5Australia@users.noreply.github.com>
2022-01-31 18:04:07 +1000

    Updating HTML files for Azure

---
+++ SiteScript.js
@@ -0,0 +1,19 @@
+function myFunction() {
+    document.getElementById("easteregg").innerHTML = "Wow! You found this :O";
+  }
+
+function twitterPs5() {
+  window.open('https://twitter.com/Ps5Australia', '_blank');
+}
+
+function twitterXsx() {
+  window.open('https://twitter.com/XSXAustralia', '_blank');
+}
+
+function twitterRat() {
+  window.open('https://twitter.com/RATStockAU', '_blank');
+}
+
+function discord() {
+  window.open('https://discord.gg/jzSfgkdsTv', '_blank');
+}
