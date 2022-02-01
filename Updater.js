283f69b45f93938440e890f7a9cacd0a49bb383c
PS5Australia <98734886+PS5Australia@users.noreply.github.com>
2022-02-01 17:02:14 +1000

    Stock

--- Updater.js
+++ Updater.js
@@ -6,15 +6,24 @@ async function OnLaunch() {
     var i = 1
     while (i < 10) {
         const url = 'https://raw.githubusercontent.com/PS5Australia/stocktrackerau/main/stock.json'
-        var response = await fetch(url);
-        var data = await response.text();
+        var response = await fetch(url, {
+            method: 'GET',
+            headers: {
+              Accept: 'application/json',
+            }
+          }
+        )
+
+        if (response.ok) {
+            var data = await response.text()
+        }
         console.log(data);
         var obj = JSON.parse(data);
-        if (obj["AmazonDisc"] == "Out Of Stock") {
-            document.getElementById("amazondisc").innerHTML="Amazon | Out Of Stock";
+        if (obj["AmazonDisc"] == "Out of Stock") {
+            document.getElementById("amazondisc").innerHTML="Amazon | Out of Stock";
         }
         else if (obj["AmazonDisc"] == "In Stock!") {
-            document.getElementById("amazondisc").innerHTML="Amazon | In Stock!";
+            document.getElementById("amazondisc").innerHTML="Amazon | In Stock";
         }
         await sleep(15000);
 
