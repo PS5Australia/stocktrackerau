398fe023b1f8e5d4bafa6a147b4ad0a101e3e504
PS5Australia <98734886+PS5Australia@users.noreply.github.com>
2022-02-02 13:30:01 +1000

    Final

--- Updater.js
+++ Updater.js
@@ -1,21 +1,3 @@
-function stockNotfication() {
-    if (!("Notification" in window)) {
-      alert("This browser does not support desktop notification");
-    }
-  
-    else if (Notification.permission === "granted") {
-      var notification = new Notification("Hi there! Send Toyu a message saying 'This Works!'");
-    }
-  
-    else if (Notification.permission !== "denied") {
-      Notification.requestPermission().then(function (permission) {
-        if (permission === "granted") {
-          var notification = new Notification("Hi there! Send Toyu a message saying 'This Works!'");
-        }
-      });
-    }
-  }
-
 async function OnLaunch() {
     const url = 'https://api.jsonbin.io/b/61f929711960493ad186160e/latest'
     var response = await fetch(url, {
@@ -35,6 +17,17 @@ async function OnLaunch() {
         else if (obj["AmazonDisc"] == "In Stock") {
             document.getElementById("amazondisc").innerHTML="Amazon | In Stock";
             document.getElementById("amazondiscborder").style.borderColor = "green";
+            if (Notification.permission === "granted") {
+                var notification = new Notification("Stock! PS5 Disc in stock at Amazon");
+              }
+            
+              else if (Notification.permission !== "denied") {
+                Notification.requestPermission().then(function (permission) {
+                  if (permission === "granted") {
+                    var notification = new Notification("Stock! PS5 Disc in stock at Amazon");
+                  }
+                });
+              }
         }
         if (obj["AmazonDigital"] == "Out of Stock") {
             document.getElementById("amazondigital").innerHTML="Amazon | Out of Stock";
@@ -42,11 +35,33 @@ async function OnLaunch() {
         else if (obj["AmazonDigital"] == "In Stock") {
             document.getElementById("amazondigital").innerHTML="Amazon | In Stock";
             document.getElementById("amazondigitalborder").style.borderColor = "green";
+            if (Notification.permission === "granted") {
+                var notification = new Notification("Stock! PS5 Digital in stock at Amazon");
+              }
+            
+              else if (Notification.permission !== "denied") {
+                Notification.requestPermission().then(function (permission) {
+                  if (permission === "granted") {
+                    var notification = new Notification("Stock! PS5 Digital in stock at Amazon");
+                  }
+                });
+              }
         }
         /*BigW*/
         if (obj["BigWDisc"] == "In Stock") {
             document.getElementById("bigwdisc").innerHTML="Big W | In Stock";
             document.getElementById("bigwdiscborder").style.borderColor = "green";
+            if (Notification.permission === "granted") {
+                var notification = new Notification("Stock! PS5 Disc in stock at Big W");
+              }
+            
+              else if (Notification.permission !== "denied") {
+                Notification.requestPermission().then(function (permission) {
+                  if (permission === "granted") {
+                    var notification = new Notification("Stock! PS5 Disc in stock at Big W");
+                  }
+                });
+              }
         }
         else if (obj["BigWDisc"] == "Out of Stock") {
             document.getElementById("bigwdisc").innerHTML="Big W | Out of Stock";
@@ -54,6 +69,17 @@ async function OnLaunch() {
         if (obj["BigWDigital"] == "In Stock") {
             document.getElementById("bigwdigital").innerHTML="Big W | In Stock";
             document.getElementById("bigwdigitalborder").style.borderColor = "green";
+            if (Notification.permission === "granted") {
+                var notification = new Notification("Stock! PS5 Digital in stock at Big W");
+              }
+            
+              else if (Notification.permission !== "denied") {
+                Notification.requestPermission().then(function (permission) {
+                  if (permission === "granted") {
+                    var notification = new Notification("Stock! PS5 Digital in stock at Big W");
+                  }
+                });
+              }
         }
         else if (obj["BigWDigital"] == "Out of Stock") {
             document.getElementById("bigwdigital").innerHTML="Big W | Out of Stock";
@@ -62,6 +88,17 @@ async function OnLaunch() {
         if (obj["TargetDigital"] == "In Stock") {
             document.getElementById("targetdigital").innerHTML="Target | In Stock";
             document.getElementById("targetdigitalborder").style.borderColor = "green";
+            if (Notification.permission === "granted") {
+                var notification = new Notification("Stock! PS5 Digital in stock at Target");
+              }
+            
+              else if (Notification.permission !== "denied") {
+                Notification.requestPermission().then(function (permission) {
+                  if (permission === "granted") {
+                    var notification = new Notification("Stock! PS5 Digital in stock at Target");
+                  }
+                });
+              }
         }
         else if (obj["TargetDigital"] == "Out of Stock") {
             document.getElementById("targetdigital").innerHTML="Target | Out of Stock";
@@ -69,6 +106,17 @@ async function OnLaunch() {
         if (obj["TargetDisc"] == "In Stock") {
             document.getElementById("targetdisc").innerHTML="Target | In Stock";
             document.getElementById("targetdiscborder").style.borderColor = "green";
+            if (Notification.permission === "granted") {
+                var notification = new Notification("Stock! PS5 Disc in stock at Target");
+              }
+            
+              else if (Notification.permission !== "denied") {
+                Notification.requestPermission().then(function (permission) {
+                  if (permission === "granted") {
+                    var notification = new Notification("Stock! PS5 Disc in stock at Target");
+                  }
+                });
+              }
         }
         else if (obj["TargetDisc"] == "Out of Stock") {
             document.getElementById("targetdisc").innerHTML="Target | Out of Stock";
@@ -76,6 +124,17 @@ async function OnLaunch() {
         if (obj["TargetBundle"] == "In Stock") {
             document.getElementById("targetdiscbundle").innerHTML="Target | In Stock";
             document.getElementById("targetbundleborder").style.borderColor = "green";
+            if (Notification.permission === "granted") {
+                var notification = new Notification("Stock! PS5 Disc Bundle in stock at Target");
+              }
+            
+              else if (Notification.permission !== "denied") {
+                Notification.requestPermission().then(function (permission) {
+                  if (permission === "granted") {
+                    var notification = new Notification("Stock! PS5 Disc Bundle in stock at Target");
+                  }
+                });
+              }
         }
         else if (obj["TargetBundle"] == "Out of Stock") {
             document.getElementById("targetdiscbundle").innerHTML="Target | Out of Stock";
@@ -84,6 +143,17 @@ async function OnLaunch() {
         if (obj["SonyDiscBundle"] == "In Stock") {
             document.getElementById("sonydiscbundle").innerHTML="Sony | In Stock";
             document.getElementById("sonydiscbundleborder").style.borderColor = "green";
+            if (Notification.permission === "granted") {
+                var notification = new Notification("Stock! PS5 Disc Bundle in stock at Sony");
+              }
+            
+              else if (Notification.permission !== "denied") {
+                Notification.requestPermission().then(function (permission) {
+                  if (permission === "granted") {
+                    var notification = new Notification("Stock! PS5 Disc Bundle in stock at Sony");
+                  }
+                });
+              }
         }
         else if (obj["SonyDiscBundle"] == "Out of Stock") {
             document.getElementById("sonydiscbundle").innerHTML="Sony | Out of Stock";
@@ -91,6 +161,17 @@ async function OnLaunch() {
         if (obj["SonyDigitalBundle"] == "In Stock") {
             document.getElementById("sonydigitalbundle").innerHTML="Sony | In Stock";
             document.getElementById("sonydigitalbundleborder").style.borderColor = "green";
+            if (Notification.permission === "granted") {
+                var notification = new Notification("Stock! PS5 Digital Bundle in stock at Sony");
+              }
+            
+              else if (Notification.permission !== "denied") {
+                Notification.requestPermission().then(function (permission) {
+                  if (permission === "granted") {
+                    var notification = new Notification("Stock! PS5 Digital Bundle in stock at Sony");
+                  }
+                });
+              }
         }
         else if (obj["SonyDigitalBundle"] == "Out of Stock") {
             document.getElementById("sonydigitalbundle").innerHTML="Sony | Out of Stock";
@@ -98,6 +179,17 @@ async function OnLaunch() {
         if (obj["SonyDigital"] == "In Stock") {
             document.getElementById("sonydigital").innerHTML="Sony | In Stock";
             document.getElementById("sonydigitalborder").style.borderColor = "green";
+            if (Notification.permission === "granted") {
+                var notification = new Notification("Stock! PS5 Digital in stock at Sony");
+              }
+            
+              else if (Notification.permission !== "denied") {
+                Notification.requestPermission().then(function (permission) {
+                  if (permission === "granted") {
+                    var notification = new Notification("Stock! PS5 Digital in stock at Sony");
+                  }
+                });
+              }
         }
         else if (obj["SonyDigital"] == "Out of Stock") {
             document.getElementById("sonydigital").innerHTML="Sony | Out of Stock";
@@ -105,6 +197,17 @@ async function OnLaunch() {
         if (obj["SonyDisc"] == "In Stock") {
             document.getElementById("sonydisc").innerHTML="Sony | In Stock";
             document.getElementById("sonydiscborder").style.borderColor = "green";
+            if (Notification.permission === "granted") {
+                var notification = new Notification("Stock! PS5 Disc in stock at Sony");
+              }
+            
+              else if (Notification.permission !== "denied") {
+                Notification.requestPermission().then(function (permission) {
+                  if (permission === "granted") {
+                    var notification = new Notification("Stock! PS5 Disc in stock at Sony");
+                  }
+                });
+              }
         }
         else if (obj["SonyDisc"] == "Out of Stock") {
             document.getElementById("sonydisc").innerHTML="Sony | Out of Stock";
