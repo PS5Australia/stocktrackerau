66a5fa41c228e478cb418f6f64e687bfae90fb92
PS5Australia <98734886+PS5Australia@users.noreply.github.com>
2022-02-02 10:59:20 +1000

    Test

--- Updater.js
+++ Updater.js
@@ -1,4 +1,23 @@
+function stockNotfication() {
+    if (!("Notification" in window)) {
+      alert("This browser does not support desktop notification");
+    }
+  
+    else if (Notification.permission === "granted") {
+      var notification = new Notification("Hi there! Send Toyu a message saying 'This Works!'");
+    }
+  
+    else if (Notification.permission !== "denied") {
+      Notification.requestPermission().then(function (permission) {
+        if (permission === "granted") {
+          var notification = new Notification("Hi there! Send Toyu a message saying 'This Works!'");
+        }
+      });
+    }
+  }
+
 async function OnLaunch() {
+    stockNotfication()
     const url = 'https://api.jsonbin.io/b/61f929711960493ad186160e/latest'
     var response = await fetch(url, {
         method: 'GET',
@@ -16,22 +35,26 @@ async function OnLaunch() {
         }
         else if (obj["AmazonDisc"] == "In Stock") {
             document.getElementById("amazondisc").innerHTML="Amazon | In Stock";
+            document.getElementById("amazondiscborder").style.borderColor = "green";
         }
         if (obj["AmazonDigital"] == "Out of Stock") {
             document.getElementById("amazondigital").innerHTML="Amazon | Out of Stock";
         }
         else if (obj["AmazonDigital"] == "In Stock") {
             document.getElementById("amazondigital").innerHTML="Amazon | In Stock";
+            document.getElementById("amazondigitalborder").style.borderColor = "green";
         }
         /*BigW*/
         if (obj["BigWDisc"] == "In Stock") {
             document.getElementById("bigwdisc").innerHTML="Big W | In Stock";
+            document.getElementById("bigwdiscborder").style.borderColor = "green";
         }
         else if (obj["BigWDisc"] == "Out of Stock") {
             document.getElementById("bigwdisc").innerHTML="Big W | Out of Stock";
         }
         if (obj["BigWDigital"] == "In Stock") {
             document.getElementById("bigwdigital").innerHTML="Big W | In Stock";
+            document.getElementById("bigwdigitalborder").style.borderColor = "green";
         }
         else if (obj["BigWDigital"] == "Out of Stock") {
             document.getElementById("bigwdigital").innerHTML="Big W | Out of Stock";
@@ -39,43 +62,50 @@ async function OnLaunch() {
         /*Target*/
         if (obj["TargetDigital"] == "In Stock") {
             document.getElementById("targetdigital").innerHTML="Target | In Stock";
+            document.getElementById("targetdigitalborder").style.borderColor = "green";
         }
         else if (obj["TargetDigital"] == "Out of Stock") {
             document.getElementById("targetdigital").innerHTML="Target | Out of Stock";
         }
         if (obj["TargetDisc"] == "In Stock") {
             document.getElementById("targetdisc").innerHTML="Target | In Stock";
+            document.getElementById("targetdiscborder").style.borderColor = "green";
         }
         else if (obj["TargetDisc"] == "Out of Stock") {
             document.getElementById("targetdisc").innerHTML="Target | Out of Stock";
         }
-        if (obj["TargetDiscBundle"] == "In Stock") {
+        if (obj["TargetBundle"] == "In Stock") {
             document.getElementById("targetdiscbundle").innerHTML="Target | In Stock";
+            document.getElementById("targetbundleborder").style.borderColor = "green";
         }
-        else if (obj["TargetDiscBundle"] == "Out of Stock") {
-            document.getElementById("targetdiscdundle").innerHTML="Target | Out of Stock";
+        else if (obj["TargetBundle"] == "Out of Stock") {
+            document.getElementById("targetdiscbundle").innerHTML="Target | Out of Stock";
         }
         /*Sony*/
         if (obj["SonyDiscBundle"] == "In Stock") {
             document.getElementById("sonydiscbundle").innerHTML="Sony | In Stock";
+            document.getElementById("sonydiscbundleborder").style.borderColor = "green";
         }
         else if (obj["SonyDiscBundle"] == "Out of Stock") {
             document.getElementById("sonydiscbundle").innerHTML="Sony | Out of Stock";
         }
         if (obj["SonyDigitalBundle"] == "In Stock") {
             document.getElementById("sonydigitalbundle").innerHTML="Sony | In Stock";
+            document.getElementById("sonydigitalbundleborder").style.borderColor = "green";
         }
         else if (obj["SonyDigitalBundle"] == "Out of Stock") {
             document.getElementById("sonydigitalbundle").innerHTML="Sony | Out of Stock";
         }
         if (obj["SonyDigital"] == "In Stock") {
             document.getElementById("sonydigital").innerHTML="Sony | In Stock";
+            document.getElementById("sonydigitalborder").style.borderColor = "green";
         }
         else if (obj["SonyDigital"] == "Out of Stock") {
             document.getElementById("sonydigital").innerHTML="Sony | Out of Stock";
         }
         if (obj["SonyDisc"] == "In Stock") {
             document.getElementById("sonydisc").innerHTML="Sony | In Stock";
+            document.getElementById("sonydiscborder").style.borderColor = "green";
         }
         else if (obj["SonyDisc"] == "Out of Stock") {
             document.getElementById("sonydisc").innerHTML="Sony | Out of Stock";
