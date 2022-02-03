7837e001929961445ec439a0ab7d40296b6ee984
PS5Australia <98734886+PS5Australia@users.noreply.github.com>
2022-02-03 10:57:17 +1000

    fix

--- contact-form-process.php
+++ contact-form-process.php
@@ -8,10 +8,10 @@
     $email_body = "Name: $name.\n".
                     "Email: $name.\n".
                         "Message: $name.\n".;
-    
+
     $to = "contact@stocktrackerau.com";
     $headers = "From: $email_from \r\n";
     $headers .= "Reply-To: $visitor_email \r\n";
     mail($to,$email_subject,$email_body,$headers);
-    
     header("Location: index.html");
+?>
