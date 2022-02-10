f864bfebe8589862ac5007c9cca603cef0e8f9f4
PS5Australia <98734886+PS5Australia@users.noreply.github.com>
2022-02-10 17:57:43 +1000

    S

--- updater.php
+++
@@ -1,7 +0,0 @@
-<?php
-$jsonString = file_get_contents('livestock.json');
-$data = json_decode($jsonString, true);
-$data["AmazonDisc"] = "In Stock";
-$newJsonString = json_encode($data);
-file_put_contents('livestock.json', $newJsonString);
-?>
